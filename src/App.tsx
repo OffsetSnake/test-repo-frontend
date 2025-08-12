3ff06c5911f56b68aa875a4523cb0643d271008c
Bogdanov Artem <artem.bogdanov.199999@gmail.com>
2025-08-12 12:56:00 +0300

    Corrected the conditions for displaying pagination

--- src/App.tsx
+++ src/App.tsx
@@ -34,25 +34,26 @@ function App() {
   return (
     <div class="max-w-5xl mx-auto p-6">
       <h1 class="text-2xl font-bold mb-6 text-center text-black">Наши Боты</h1>
-
-      <div class="mb-4 flex justify-center gap-4 items-center">
-        <span>Показывать по:</span>
-        {[4, 8, 12].map((num) => (
-          <button
-            class={`px-3 py-1 rounded ${
-              limit() === num
-                ? "bg-blue-600 text-white"
-                : "bg-gray-200 text-gray-700"
-            }`}
-            onClick={() => {
-              setPage(1);
-              setLimit(num);
-            }}
-          >
-            {num}
-          </button>
-        ))}
-      </div>
+      <Show when={!query.isLoading && !query.isError}>
+        <div class="mb-4 flex justify-center gap-4 items-center">
+          <span>Показывать по:</span>
+          {[4, 8, 12].map((num) => (
+            <button
+              class={`px-3 py-1 rounded ${
+                limit() === num
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-200 text-gray-700"
+              }`}
+              onClick={() => {
+                setPage(1);
+                setLimit(num);
+              }}
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+      </Show>
 
       <Show
         when={!query.isError}
@@ -71,39 +72,40 @@ function App() {
           </ul>
         </Show>
       </Show>
+      <Show when={!query.isLoading && !query.isError}>
+        <div class="flex justify-center mt-6 gap-2">
+          <button
+            class="px-3 py-1 rounded border disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={page() === 1}
+            onClick={() => setPage(page() - 1)}
+          >
+            Назад
+          </button>
 
-      <div class="flex justify-center mt-6 gap-2">
-        <button
-          class="px-3 py-1 rounded border disabled:opacity-50 disabled:cursor-not-allowed"
-          disabled={page() === 1}
-          onClick={() => setPage(page() - 1)}
-        >
-          Назад
-        </button>
+          <For each={Array.from({ length: totalPages() }, (_, i) => i + 1)}>
+            {(pageNum) => (
+              <button
+                class={`px-3 py-1 rounded border ${
+                  page() === pageNum
+                    ? "bg-blue-600 text-white"
+                    : "bg-white text-gray-700 hover:bg-gray-100"
+                }`}
+                onClick={() => setPage(pageNum)}
+              >
+                {pageNum}
+              </button>
+            )}
+          </For>
 
-        <For each={Array.from({ length: totalPages() }, (_, i) => i + 1)}>
-          {(pageNum) => (
-            <button
-              class={`px-3 py-1 rounded border ${
-                page() === pageNum
-                  ? "bg-blue-600 text-white"
-                  : "bg-white text-gray-700 hover:bg-gray-100"
-              }`}
-              onClick={() => setPage(pageNum)}
-            >
-              {pageNum}
-            </button>
-          )}
-        </For>
-
-        <button
-          class="px-3 py-1 rounded border disabled:opacity-50 disabled:cursor-not-allowed"
-          disabled={page() === totalPages()}
-          onClick={() => setPage(page() + 1)}
-        >
-          Вперед
-        </button>
-      </div>
+          <button
+            class="px-3 py-1 rounded border disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={page() === totalPages()}
+            onClick={() => setPage(page() + 1)}
+          >
+            Вперед
+          </button>
+        </div>
+      </Show>
     </div>
   );
 }
